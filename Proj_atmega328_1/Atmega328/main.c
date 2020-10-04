9e307278c271ee71d717bb35e26a049fed6fb901
sergio1020881 <sergio.salazar.santos@gmail.com>
2020-10-04 17:40:57 +0100

    atemga328_1

---
+++ Proj_atmega328_1/Atmega328/main.c
@@ -0,0 +1,18 @@
+/*
+ * Atmega328.c
+ *
+ * Created: 04/10/2020 16:37:53
+ * Author : sergi
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

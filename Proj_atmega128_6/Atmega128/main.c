95504e61036d59a3d57dcbe9aa71bb5bf5165a2e
sergio1020881 <sergio.salazar.santos@gmail.com>
2020-10-09 20:31:05 +0100

    trial_rtc_pcf8563

---
+++ Proj_atmega128_6/Atmega128/main.c
@@ -0,0 +1,18 @@
+/*
+ * Atmega128.c
+ *
+ * Created: 09/10/2020 20:25:13
+ * Author : sergi
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

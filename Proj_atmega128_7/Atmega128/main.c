d74cb576a04005abeba8f7430a820ca329f334b2
sergio1020881 <sergio.salazar.santos@gmail.com>
2020-10-11 18:35:26 +0100

    11102020_3_new_test_rig

---
+++ Proj_atmega128_7/Atmega128/main.c
@@ -0,0 +1,36 @@
+/*
+	Atmega128.c
+Created: 09/10/2020 20:25:13
+Author: Sérgio Santos
+	<sergio.salazar.santos@gmail.com>
+Comment:
+	Standby
+ */
+/***Preamble***/
+#define F_CPU 16000000UL
+/***Library***/
+#include <avr/io.h>
+#include <util/delay.h>
+#include "lcd.h"
+#include "function.h"
+/***Define and Macros***/
+#define TRUE 1
+#define ZERO 0
+/***Global Variable***/
+/***MAIN_MAIN***/
+int main(void)
+{
+	//uint8_t i;
+	//LCD0 lcd = LCD0enable(&DDRA,&PINA,&PORTA);
+	//FUNC func = FUNCenable();
+	/* Replace with your application code */
+	while (1)
+    {    //lcd.reboot();
+		
+		
+		
+		
+    }
+}
+/*EOF*/
+
